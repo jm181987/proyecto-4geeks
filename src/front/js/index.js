ad89b7ef9feddeee1dca41ce6bd071382e5681f0
jm181987 <jorgitom18@gmail.com>
2023-01-13 15:46:19 +0000

    login inicia

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -8,5 +8,22 @@ import "../styles/index.css";
 //import your own components
 import Layout from "./layout";
 
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="grupoalpha.us.auth0.com"
+    clientId="IipsWH6R7yq12ryfBygVNW1WbJFu2mX2"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('app')
+);
+
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
