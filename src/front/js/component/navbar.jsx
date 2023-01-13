a99bf6ba2a5db7ae8c51c3d3dfaa854a0734b2be
jm181987 <jorgitom18@gmail.com>
2023-01-13 17:16:12 +0000

    ok

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -14,14 +14,14 @@ export const Navbar = () => {
   
   return (
 		<nav className="navbar navbar-dark bg-dark mb-3">
-		<nav className="navbar navbar-expand-md bg-dark">
+		<nav className="navbar navbar-expand-md bg-dark ">
   <div className="container-fluid">
     <div id="logo" >
     <img src={logo} />
     </div>
     <a className="navbar-brand " href="#"></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon "></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,16 +2,36 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import logo from "/workspace/proyecto-4geeks/docs/assets/logo.png"
+
+
+
+const LoginButton = () => {
+	const { loginWithRedirect } = useAuth0();
+  
+	return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  };
+  
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
+		
+
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
+			<h1>test</h1>
+			<div></div>
+			<p> 
+			
+				<img src={logo} />
 			</p>
+			<div> 
+
+
+			</div>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
@@ -21,6 +41,10 @@ export const Home = () => {
 					Read documentation
 				</a>
 			</p>
+		
 		</div>
+		
+		
 	);
 };
+export default LoginButton;
