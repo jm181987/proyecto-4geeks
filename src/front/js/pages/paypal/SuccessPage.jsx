4c25f0ceb4ed98e77cbc03ef841a8a19c62c5da5
luisferdonoso <luisfer.donoso@gmail.com>
2023-02-10 01:38:26 +0000

    CAmbios de Estilo paypal success

--- src/front/js/pages/paypal/SuccessPage.jsx
+++ src/front/js/pages/paypal/SuccessPage.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
 import Navbar from '/workspace/proyecto-4geeks/src/front/js/component/navbar.jsx';
-import Footer from '/workspace/proyecto-4geeks/src/front/js/component/footer.jsx';
+
 
 import '/workspace/proyecto-4geeks/src/paypal/SeccessPage.css';
-import { Button } from 'react-bootstrap';
+
 
 
 export const SuccessPage = (props) => {
   const {phone} = props
   
   return (
-    <div className='container-fluid'>
-      <h1 classNme='row align-items-center'>Felicidades , tu transacción ha sido un éxito!</h1>
-      <h2>
+    <div className='contenedor '>
+      <h1 id="titulo-success" >Tu transacción ha sido un éxito!</h1>
+      <h2 id="texto-success">
         Contacta con tu artista:
       </h2>
-     <a href="https://api.whatsapp.com/send?phone=992647971"><button class="shadow__btn1 justify-content-center align-items-center">Contactar</button></a>
+     <a id="boton-success" href="https://api.whatsapp.com/send?phone=992647971"><button class="shadow__btn1">Contactar</button></a>
       
       
       
